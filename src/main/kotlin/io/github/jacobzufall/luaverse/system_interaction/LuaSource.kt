49ab65d8d771d736dc733a2699a644fb325e6508
Jacob Zufall <jacobzufall@gmail.com>
2024-12-18 19:19:03 -0500

    Worked on adding webscraping so that the CLI can download from Lua.org

--- src/main/kotlin/io/github/jacobzufall/luaverse/system_interaction/LuaSource.kt
+++ src/main/kotlin/io/github/jacobzufall/luaverse/system_interaction/LuaSource.kt
@@ -1,15 +1,49 @@
 package io.github.jacobzufall.luaverse.system_interaction
 
-import io.github.jacobzufall.luaverse.Settings
 import java.io.File
 
-class LuaSource(path: String) {
-    private val luaFolder: String = path.split("\\").last()
-    private val luaVersion: String = luaFolder.split("-").last()
+// I followed this tutorial for web scraping: https://www.zenrows.com/blog/kotlin-web-scraping#request
+import it.skrape.core.*
+import it.skrape.fetcher.*
+
+import io.github.jacobzufall.luaverse.Settings
+
+class LuaSource(path: String = "") {
+    var luaVersionFiles: MutableMap<String, String> = mutableMapOf()
+
+    private val luaFtp = skrape(HttpFetcher) {
+        request { url = "https://www.lua.org/ftp/" }
+
+        response {
+            htmlDocument {
+                val files = findAll("a")
+                    .filter { it.hasAttribute("href") }
+                    .map { element ->
+                        val name = element.text
+                        val href = element.attribute("href")
+                        name to href
+                    }
+
+                files.forEach {(name, href) ->
+                    if (name.startsWith("lua") && !name.startsWith("lua-all")) {
+                        // This doesn't work for older versions that only had two numbers.
+                        val version: String = name.substring(4, 9)
+                        luaVersionFiles[version] = href
+                    }
+                }
+            }
+        }
+    }
+
 
     fun build() {
 
     }
+
+
+    // Everything below this comment will be deprecated.
+    private val luaFolder: String = path.split("\\").last()
+    private val luaVersion: String = luaFolder.split("-").last()
 
     fun build_old(): Boolean {
         val command: String = String.format("build.bat \"%s\" \"%s\"", luaVersion, Settings.directories["build"])
@@ -35,5 +69,12 @@ class LuaSource(path: String) {
 
         // Temporary. Should return true or false based on the success of the build.
         return true
+    }
+}
+
+fun main() {
+    val luaSource: LuaSource = LuaSource()
+    for ((name, link) in luaSource.luaVersionFiles) {
+        println("$name | $link")
     }
 }
