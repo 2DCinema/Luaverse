abe8db0351a73f2691dd7e93dc5050fa2b0e90a5
Jacob Zufall <jacobzufall@gmail.com>
2024-12-27 16:50:15 -0500

    Work on VersionString.kt

--- src/main/kotlin/io/github/jacobzufall/luaverse/utility/VersionString.kt
+++ src/main/kotlin/io/github/jacobzufall/luaverse/utility/VersionString.kt
@@ -1,13 +1,28 @@
 package io.github.jacobzufall.luaverse.utility
 
-class VersionString(delimited_version: String) {
+class VersionString(version: String) {
     /*
     A user can input a version number in any format, so long as it delimited consistently. For example, 1.2.3 is okay,
     since it is delimited, and it only has one delimiter. 1.2-3 and 123 are not okay, but 1.24.1 and 1-24-1 are.
     */
+
+    /*
+    Converts it to a set so there's only one of each value, and then removes the numbers from that set to return
+    a list with a size of 1, which we then get the 0th index of to find the delimiter.
+    */
+    private val delimiter: String = version.toSet().filter { !it.isDigit() }[0].toString()
+    // Then, we turn "1.2.10" into "[1, 2, 10]".
+    private val versionNumbers: List<String> = version.split(delimiter)
+
     init {
-        val uniqueChars: Set<Char> = delimited_version.toSet()
+        /**
+         * The version in delimited format (1.2.3).
+         */
+        var delimitedVersion: String = versionNumbers.joinToString(delimiter)
 
-
+        /**
+         * The version in raw format (123).
+         */
+        var rawVersion: String = versionNumbers.joinToString("")
     }
 }
